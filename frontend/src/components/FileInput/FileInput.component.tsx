e92aad3b9dde3fe611886ef959679368ae79ea12
nicoDeheza <nicolasdeheza@hotmail.com>
2026-02-17 16:55:29 +0100

    add file input

---
+++ frontend/src/components/FileInput/FileInput.component.tsx
@@ -0,0 +1,45 @@
+import {useRef, type FC, type ReactNode, type ChangeEvent} from 'react'
+import {Button} from '../Button/Button.component'
+import styles from './FileInput.module.css'
+
+interface Props {
+	onFileChange: (file: File) => void
+	accept: string
+	children: ReactNode
+	disabled?: boolean
+}
+
+export const FileInput: FC<Props> = ({
+	onFileChange,
+	accept,
+	children,
+	disabled = false
+}) => {
+	const inputRef = useRef<HTMLInputElement>(null)
+
+	const handleButtonClick = () => {
+		inputRef.current?.click()
+	}
+
+	const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const selectedFile = e.target.files?.[0]
+		if (!selectedFile) return
+		onFileChange(selectedFile)
+	}
+
+	return (
+		<>
+			<input
+				ref={inputRef}
+				type="file"
+				accept={accept}
+				onChange={handleFileChange}
+				disabled={disabled}
+				className={styles.fileInput}
+			/>
+			<Button onClick={handleButtonClick} disabled={disabled}>
+				{children}
+			</Button>
+		</>
+	)
+}
